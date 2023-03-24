86542651f1655392cb6d0fc67366bfe59ca82f1a
JosemiGuerrero <j.guerrero.trejo@iescristobaldemonroy.es>
2023-03-24 09:54:03 +0100

    segundo commit

--- evaluacion2/src/Evaluacion2/TablaEnteros.java
+++ evaluacion2/src/Evaluacion2/TablaEnteros.java
@@ -7,19 +7,31 @@ package Evaluacion2;
  * 
  * @author DAM-1
  * @since 24/3/2023
+ * @see <a href="https://github.com/JosemiGuerrero/evaluacion2">Ver Repositorio Git</a>
  */
 
 public class TablaEnteros {
 	
 	private Integer[] tabla;
 
+	/**
+	 * Constructor de la clase TablaEnteros. Se le tiene que pasar un array de Enteros
+	 * Se comprueba que el array no esté vacío
+	 * 
+	 * @param tabla Array de ints
+	 * @throws IllegalArgumentException si el Array está vacío
+	 */
 	TablaEnteros(Integer[] tabla) {
 		if (tabla == null || tabla.length == 0)
 			throw new IllegalArgumentException("No hay elementos");
 		this.tabla = tabla;
 	}
 
-	// devuelve la suma de los elementos de la tabla
+	/**
+	 * Devuelve la suma de los elementos de la tabla
+	 * 
+	 * @return int con la suma de todos los elementos de la tabla
+	 */
 	public int sumaTabla() {
 		int suma = 0;
 		for (int i = 0; i < tabla.length; i++)
@@ -27,7 +39,11 @@ public class TablaEnteros {
 		return suma;
 	}
 
-	// devuelve el mayor elemento de la tabla
+	/**
+	 * devuelve el mayor elemento de la tabla
+	 * 
+	 * @return int con el mayor valor de los elementos de la tabla
+	 */
 	public int mayorTabla() {
 		int max = -999;
 		for (int i = 0; i < tabla.length; i++)
@@ -36,7 +52,14 @@ public class TablaEnteros {
 		return max;
 	}
 
-	// devuelve la posici�n de un elemento cuyo valor se pasa
+	/**
+	 * Devuelve la posición de un elemento cuyo valor se pasa
+	 * Controla que el valro a buscar no se encuentre en la tabla
+	 * 
+	 * @param n valor a buscar
+	 * @throws NoSuchElementException en caso de que el valor no exista en la tabla
+	 * @return int con la posicion del elemento en la tabla
+	 */
 	public int posicionTabla(int n) {
 		for (int i = 0; i < tabla.length; i++)
 			if (tabla[i] == n)
